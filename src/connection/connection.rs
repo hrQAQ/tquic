1da4c7d00a697958c9292962a85aa9fcb852f05b
dzj <2091088157.qq.com>
2025-09-09 20:56:34 +0800

    fix delay

--- src/connection/connection.rs
+++ src/connection/connection.rs
@@ -2640,7 +2640,7 @@ impl Connection {
         let mut cap: usize = out.len();
 
         while let Some((frame_priority, stream_id)) = self.streams.peek_sendable() {
-            if frame_priority<datagram_priotrity && !is_lower
+            if frame_priority<datagram_priotrity && !is_lower  || !self.datagram_map.if_out_empty()
             {
                 // next run try_write_stream_frame
                 info!("the {} stream frame priority lower than datagram, so all stream delay",stream_id);
